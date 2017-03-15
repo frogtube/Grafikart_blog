fde15aa884370409e2fb212efad7264ffcd07286
frogtube <u.pradere@gmail.com>
2017-03-15 20:08:29 +0100

    Post CRUD added --half video: le crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  root 'pages#salut'
+  resources :posts
+
+  root 'posts#index'
   get '/salut(/:name)', to: 'pages#salut', as: 'salut'
   get '/about', to: 'pages#about', as: 'about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
